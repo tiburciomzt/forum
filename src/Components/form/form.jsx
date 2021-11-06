fe0b2956b916d115a232b174af7d98ebdd26d4f3
Jonathan Oskar Hernandez <jonathan.oskar.hernandez@ibm.com>
2021-11-06 13:53:02 -0600

    update form

--- src/Components/form/form.jsx
+++ src/Components/form/form.jsx
@@ -5,14 +5,39 @@ import './form.css';
 const Form = () => {
     return (
         <div className='form'>
-            <form>
+            <form method="GET">
                 <label>
-                user:
-                <input type="text" name="name" />
-                SRC:
-                <input type="text" src="src" />
-                </label>
-                <input type="submit" value="Submit" />
+                    
+                    <input type="text" name="name" placeholder="User" />
+                    Date:
+                    <input type="date" placeholder="User"/><br/>
+                    Title:
+                    <input type="text" name="title" />
+                    SRC:
+                    <input type="text" c="src" />
+                    Program:
+                    <input type="text" name="prog" /><br/>
+                    Sistem:
+                    <select name="sistem">
+                        <option  value="mowgly">Mowgly</option>
+                        <option value="zz">ZZ</option>
+                        <option value="fleetwood">Fleetwood</option>
+                        <option value="denali">Denali</option>
+                        <option value="tuleta">Tuleta</option>
+                        <option value="mihawk">mihawk</option>
+                        <option value="solution">solution</option>
+                    </select>
+                    SO:
+                    <select name="so">
+                        <option>none</option>
+                        <option value="linux">Linux</option>
+                        <option value="ide">IDE</option>
+                        <option value="aix">AIX</option>
+                    </select><br/>
+                    Text:
+                    <input type="text" name="body"/><br/>
+                    <input type="submit" value="Submit" />
+                </label>           
             </form>
         </div>
     );
