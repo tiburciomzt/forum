bc4bf04ac68e026fc7f2e8225912cdcc59f9c1e7
Jonathan Oskar Hernandez <jonathan.oskar.hernandez@ibm.com>
2021-11-15 16:27:44 -0600

    update form

--- src/Components/form/form.jsx
+++ src/Components/form/form.jsx
@@ -7,36 +7,40 @@ const Form = () => {
         <div className='form'>
             <form method="GET">
                 <label>
-                    
-                    <input type="text" name="name" placeholder="User" />
-                    Date:
-                    <input type="date" placeholder="User"/><br/>
-                    Title:
-                    <input type="text" name="title" />
-                    SRC:
-                    <input type="text" c="src" />
-                    Program:
-                    <input type="text" name="prog" /><br/>
-                    Sistem:
-                    <select name="sistem">
-                        <option  value="mowgly">Mowgly</option>
-                        <option value="zz">ZZ</option>
-                        <option value="fleetwood">Fleetwood</option>
-                        <option value="denali">Denali</option>
-                        <option value="tuleta">Tuleta</option>
-                        <option value="mihawk">mihawk</option>
-                        <option value="solution">solution</option>
-                    </select>
-                    SO:
-                    <select name="so">
-                        <option>none</option>
-                        <option value="linux">Linux</option>
-                        <option value="ide">IDE</option>
-                        <option value="aix">AIX</option>
-                    </select><br/>
-                    Text:
-                    <input type="text" name="body"/><br/>
-                    <input type="submit" value="Submit" />
+                <div><p>FORM</p></div>
+                    <div>
+                        <input type="text" name="name" placeholder="User" /> 
+                        <input type="text" name="src" placeholder="SRC" />
+                        <input type="text" name="prog" placeholder="Program" /><br/>
+                    </div>
+                    <div className="form_selectors">
+                        <label>Sistem:</label>    
+                        <select  name="sistem">
+                            <option  value="none">-----</option>
+                            <option  value="mowgly">Mowgly</option>
+                            <option value="zz">ZZ</option>
+                            <option value="fleetwood">Fleetwood</option>
+                            <option value="denali">Denali</option>
+                            <option value="tuleta">Tuleta</option>
+                            <option value="mihawk">mihawk</option>
+                            <option value="solution">solution</option>
+                        </select>
+                        <label>SO:</label>
+                        <select  name="so">
+                            <option value="none">-----</option>
+                            <option value="linux">Linux</option>
+                            <option value="ide">IDE</option>
+                            <option value="aix">AIX</option>
+                        </select><br/>
+                    </div>
+                    <div>
+                        <input type="text" name="title" placeholder="Title" />
+                        <input id="message" type="text" placeholder="Message"></input>
+                    </div>
+                    <div >
+                        <input className="button" type="submit" value="Save" />
+                        <input className="button"  type="submit" value="Return" />
+                    </div>
                 </label>           
             </form>
         </div>
